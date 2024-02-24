d10da06cb9dbcfafefe974e1536ec89c325c917c
Farhan Shaikh <thinkingdatascience@gmail.com>
2024-02-24 23:52:11 +0300

    exception added_1

--- us_visa/exception/__init__.py
+++ us_visa/exception/__init__.py
@@ -16,3 +16,6 @@ class USvisaException(Exception):
         self.error_message = error_message_details(
             error_message, error_details=error_details
         )
+
+    def __str__(self):
+        return self.error_message
