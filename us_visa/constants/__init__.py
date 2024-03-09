aa26ad8790dfb6e69a552a7a2be74de6cab9fb3b
Farhan Shaikh <thinkingdatascience@gmail.com>
2024-03-09 09:21:45 +0300

    data validation added

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -1,13 +1,13 @@
 import os
 from datetime import date
 
-# Constant for MongoDB
+# CONSTANT FOR MONGODB
 MONGODB_URL_KEY = "MONGODB_URL"
 DATABASE_NAME = "US_VISA"
 COLLECTION_NAME = "visa_data"
 
 
-# Other Constant
+# OTHER CONSTANT
 PIPELINE_NAME: str = "usvisa"
 ARTIFACT_DIR: str = "artifact"
 
@@ -15,19 +15,25 @@ MODEL_FILE_NAME = "model.pkl"
 PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
 
 
-TARGET_COLUMN = "case_status"  # Dependent Variable
+TARGET_COLUMN = "case_status"  # DEPENDENT VARIABLE
 CURRENT_YEAR = date.today().year
 
 FILE_NAME: str = "usvisa.csv"
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
+SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
 
-"""
-Data Ingestion related constant starts with "DATA_INGESTION" and Variable Name
-"""
+
+# DATA INGESTION RELATED CONSTANT STARTS WITH "DATA_INGESTION" AND THE VARIABLE NAME
 
 DATA_INGESTION_COLLECTION_NAME: str = "visa_data"
 DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
+
+
+# DATA VALIDATION RELATED CONSTANT STARTS WITH "DATA_VALIDATION" AND THE VARIABLE NAME
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report_yaml"
--- us_visa/pipeline/training_pipeline.py
+++ us_visa/pipeline/training_pipeline.py
@@ -3,8 +3,12 @@ from us_visa.logger import logging
 from us_visa.exception import USvisaException
 
 from us_visa.components.data_ingestion import DataIngestion
-from us_visa.entity.config_entity import DataIngestionConfig
-from us_visa.entity.artifact_entity import DataIngestionArtifact
+from us_visa.components.data_validation import DataValidation
+
+from us_visa.entity.config_entity import DataIngestionConfig, DataValidationConfig
+
+
+from us_visa.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
 
 
 class TrainPipeline:
@@ -12,6 +16,7 @@ class TrainPipeline:
 
     def __init__(self) -> None:
         self.data_ingestion_config = DataIngestionConfig()
+        self.data_validation_config = DataValidationConfig()
 
     def start_data_ingestion(self) -> DataIngestionArtifact:
         try:
@@ -28,13 +33,34 @@ class TrainPipeline:
         except Exception as e:
             raise USvisaException(e, sys) from e
 
-    def run_pipeline(
-        self,
-    ) -> None:
+    def start_data_validation(
+        self, data_ingestion_artifact: DataIngestionArtifact
+    ) -> DataValidationArtifact:
+        logging.info("Entered the start of Data Validation method of Training Pipeline")
+        try:
+            data_validation = DataValidation(
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_validation_config=self.data_validation_config,
+            )
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+            logging.info("Performed the data validation opertation")
+            logging.info("Exited the start_data_validation method of Training Pipeline")
+
+            return data_validation_artifact
+
+        except Exception as e:
+            raise USvisaException(e, sys) from e
+
+    def run_pipeline(self) -> None:
         """
         This method of TrainingPipeline class is responsible for running complete pipeline
         """
         try:
             data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(
+                data_ingestion_artifact=data_ingestion_artifact
+            )
+
         except Exception as e:
             raise USvisaException(e, sys) from e
