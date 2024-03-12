f36b1f82a706528693d304f23cf2c5f9eede5e89
Farhan Shaikh <thinkingdatascience@gmail.com>
2024-03-12 06:36:26 +0300

    data transformation added

--- us_visa/entity/artifact_entity.py
+++ us_visa/entity/artifact_entity.py
@@ -12,3 +12,10 @@ class DataValidationArtifact:
     validation_status: bool
     message: str
     drift_report_file_path: str
+
+
+@dataclass
+class DataTransformationArtifact:
+    transformed_object_file_path: str
+    tranformed_train_file_path: str
+    tranformed_test_file_path: str
