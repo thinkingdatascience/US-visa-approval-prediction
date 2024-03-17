f2199744c37942886b9b61f4680da96aa30cb7f8
Farhan Shaikh <thinkingdatascience@gmail.com>
2024-03-17 17:01:08 +0300

    prediction pipeline and user app added

--- us_visa/pipeline/training_pipeline.py
+++ us_visa/pipeline/training_pipeline.py
@@ -1,13 +1,13 @@
 import sys
-from us_visa.logger import logging
 from us_visa.exception import USvisaException
+from us_visa.logger import logging
 
 from us_visa.components.data_ingestion import DataIngestion
 from us_visa.components.data_validation import DataValidation
 from us_visa.components.data_transformation import DataTransformation
 from us_visa.components.model_trainer import ModelTrainer
 from us_visa.components.model_evaluation import ModelEvaluation
-
+from us_visa.components.model_pusher import ModelPusher
 
 from us_visa.entity.config_entity import (
     DataIngestionConfig,
@@ -15,6 +15,7 @@ from us_visa.entity.config_entity import (
     DataTransformationConfig,
     ModelTrainerConfig,
     ModelEvaluationConfig,
+    ModelPusherConfig,
 )
 
 
@@ -24,38 +25,48 @@ from us_visa.entity.artifact_entity import (
     DataTransformationArtifact,
     ModelTrainerArtifact,
     ModelEvaluationArtifact,
+    ModelPusherArtifact,
 )
 
 
 class TrainPipeline:
-    """This method of TrainPipeline class is responsible for starting data ingestion component"""
-
-    def __init__(self) -> None:
+    def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
         self.data_transformation_config = DataTransformationConfig()
         self.model_trainer_config = ModelTrainerConfig()
         self.model_evaluation_config = ModelEvaluationConfig()
+        self.model_pusher_config = ModelPusherConfig()
 
     def start_data_ingestion(self) -> DataIngestionArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting data ingestion component
+        """
         try:
-            logging.info("Entered the start_data_ingestion method")
-            logging.info("Getting the data from MongoDB")
+            logging.info(
+                "Entered the start_data_ingestion method of TrainPipeline class"
+            )
+            logging.info("Getting the data from mongodb")
             data_ingestion = DataIngestion(
                 data_ingestion_config=self.data_ingestion_config
             )
             data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
-            logging.info("Got the train and test set from mongodb")
-            logging.info("Exited the start_data_ingestion method of Training Pipeline")
-
+            logging.info("Got the train_set and test_set from mongodb")
+            logging.info(
+                "Exited the start_data_ingestion method of TrainPipeline class"
+            )
             return data_ingestion_artifact
         except Exception as e:
-            raise USvisaException(e, sys)
+            raise USvisaException(e, sys) from e
 
     def start_data_validation(
         self, data_ingestion_artifact: DataIngestionArtifact
     ) -> DataValidationArtifact:
-        logging.info("Entered the start of Data Validation method of Training Pipeline")
+        """
+        This method of TrainPipeline class is responsible for starting data validation component
+        """
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
+
         try:
             data_validation = DataValidation(
                 data_ingestion_artifact=data_ingestion_artifact,
@@ -63,13 +74,17 @@ class TrainPipeline:
             )
 
             data_validation_artifact = data_validation.initiate_data_validation()
-            logging.info("Performed the data validation opertation")
-            logging.info("Exited the start_data_validation method of Training Pipeline")
+
+            logging.info("Performed the data validation operation")
+
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
 
             return data_validation_artifact
 
         except Exception as e:
-            raise USvisaException(e, sys)
+            raise USvisaException(e, sys) from e
 
     def start_data_transformation(
         self,
@@ -114,21 +129,41 @@ class TrainPipeline:
         data_ingestion_artifact: DataIngestionArtifact,
         model_trainer_artifact: ModelTrainerArtifact,
     ) -> ModelEvaluationArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting modle evaluation
+        """
         try:
             model_evaluation = ModelEvaluation(
                 model_eval_config=self.model_evaluation_config,
                 data_ingestion_artifact=data_ingestion_artifact,
                 model_trainer_artifact=model_trainer_artifact,
             )
-
             model_evaluation_artifact = model_evaluation.initiate_model_evaluation()
             return model_evaluation_artifact
         except Exception as e:
             raise USvisaException(e, sys)
 
-    def run_pipeline(self) -> None:
+    def start_model_pusher(
+        self, model_evaluation_artifact: ModelEvaluationArtifact
+    ) -> ModelPusherArtifact:
         """
-        This method of TrainingPipeline class is responsible for running complete pipeline
+        This method of TrainPipeline class is responsible for starting model pushing
+        """
+        try:
+            model_pusher = ModelPusher(
+                model_evaluation_artifact=model_evaluation_artifact,
+                model_pusher_config=self.model_pusher_config,
+            )
+            model_pusher_artifact = model_pusher.initiate_model_pusher()
+            return model_pusher_artifact
+        except Exception as e:
+            raise USvisaException(e, sys)
+
+    def run_pipeline(
+        self,
+    ) -> None:
+        """
+        This method of TrainPipeline class is responsible for running complete pipeline
         """
         try:
             data_ingestion_artifact = self.start_data_ingestion()
@@ -145,6 +180,13 @@ class TrainPipeline:
             model_evaluation_artifact = self.start_model_evaluation(
                 data_ingestion_artifact=data_ingestion_artifact,
                 model_trainer_artifact=model_trainer_artifact,
+            )
+
+            if not model_evaluation_artifact.is_model_accepted:
+                logging.info(f"Model not accepted.")
+                return None
+            model_pusher_artifact = self.start_model_pusher(
+                model_evaluation_artifact=model_evaluation_artifact
             )
 
         except Exception as e:
